f3813fc86d6ddfc797d7b75175165354f04c8f60
Leonard Pak <leopak2000@gmail.com>
2022-06-21 19:25:37 +0300

    Add all code

--- smach_controller/start.py
+++ smach_controller/start.py
@@ -1,6 +1,7 @@
 import rclpy
 import smach
 import smach_ros
+import time
 
 from rclpy.node import Node
 from std_msgs.msg import Bool
@@ -101,7 +102,7 @@ class ExecuteTask(smach.State):
 
     def execute(self, userdata):
         self.node.get_logger().info('Executing state Main Task')
-        self.StartAsyncTask()
+        self.node.robot_simulation.StartAsyncTask()
         future = self.node.get_gesture.call_async(Trigger.Request())
         curent_gesture = ''
         while rclpy.ok():
@@ -117,98 +118,147 @@ class ExecuteTask(smach.State):
                         'Result of get_face: %s' % response.message)
                     curent_gesture = response.message
                 break
-        # TODO
-        return 'async_task_start'
-
-    def StartAsyncTask():
-    # TODO
-        pass
-
-    def CheckTask():
-    # TODO
-        pass
+        if self.node.robot_simulation.IsTaskFinish():
+            return 'finish'
+        elif curent_gesture == 'stop':
+            self.start_time = None
+            return 'stop'
+        else:
+            return 'task_work_async'
 
 
 class Pause(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['start'])
+    def __init__(self, node):
+        smach.State.__init__(self, outcomes=['start', 'not_gesture'])
+        self.node = node
 
     def execute(self, userdata):
-        node.get_logger().info('Executing state Pause Task')
-        # TODO
-        return 'stop_task'
+        self. node.get_logger().info('Executing state Pause Task')
+        self.node.robot_simulation.PauseTask()
+        future = self.node.get_gesture.call_async(Trigger.Request())
+        curent_gesture = ''
+        while rclpy.ok():
+            rclpy.spin_once(self.node)
+            if future.done():
+                try:
+                    response = future.result()
+                except Exception as e:
+                    self.node.get_logger().info(
+                        'Service call failed %r' % (e,))
+                else:
+                    self.node.get_logger().info(
+                        'Result of get_face: %s' % response.message)
+                    curent_gesture = response.message
+                break
+        if curent_gesture == 'start':
+            return 'start'
+        else:
+            return 'not_gesture'
 
 
 class PrepareFinish(smach.State):
-    def __init__(self):
+    def __init__(self, node):
         smach.State.__init__(self, outcomes=['success'])
+        self.node = node
 
     def execute(self, userdata):
-        node.get_logger().info('Executing state Prepare Finish Task')
-        # TODO
+        self.node.get_logger().info('Executing state Prepare Finish Task')
+        time.sleep(10)
         return 'start_task'
+
+
+class FakeRobotSimulation():
+    def __init__(self):
+        self.start_time = None
+        self.pause_time = None
+
+    def StartAsyncTask(self):
+        if self.start_time == None:
+            self.start_time = time.perf_counter()
+        else:
+            self.start_time = self.pause_time
+            self.pause_time = None
+
+    def IsTaskFinish(self):
+        return (self.start_time - time.perf_counter) > 15.0
+
+    def PauseTask(self):
+        self.pause_time = self.start_time
+        self.start_time = None
 
 
 class MainNode(Node):
     def __init__(self):
-       super().__init__('smach_state_machine')
+        super().__init__('smach_state_machine')
 
-       self.subscription = self.create_subscription(
-           Bool,
-           'body_detection',
-           self.IsHumanCallback,
-           10)
-       self.subscription  # prevent unused variable warning
+        self.subscription = self.create_subscription(
+            Bool,
+            'body_detection',
+            self.IsHumanCallback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-       self.client_name = self.create_client(Trigger, 'get_name')
-       self.client_gesture = self.create_client(Trigger, 'get_gesture')
-       self.client_tool = self.create_client(Trigger, 'get_tool')
+        self.client_name = self.create_client(Trigger, 'get_name')
+        self.client_gesture = self.create_client(Trigger, 'get_gesture')
+        self.client_tool = self.create_client(Trigger, 'get_tool')
 
-       while (not self.client_name.wait_for_service(timeout_sec=1.0)) or (not self.client_gesture.wait_for_service(timeout_sec=1.0)) or (not self.client_tool.wait_for_service(timeout_sec=1.0)):
+        while (not self.client_name.wait_for_service(timeout_sec=1.0)) or (not self.client_gesture.wait_for_service(timeout_sec=1.0)) or (not self.client_tool.wait_for_service(timeout_sec=1.0)):
             self.get_logger().info('services not available, waiting again...')
+
+        self.robot_simalation = FakeRobotSimulation()
+
+        self.InitStateMachine()
 
     def IsHumanCallback(self, msg):
         if msg.data:
             self.get_logger().info('Human here!')
-            self.outcome = self.sm.execute()
+            self.StartStateMachin()
         else:
             self.get_logger().info('Human go out!')
-            # TODO
+            self.StopStateMachin()
 
-    def InitStateMachine():
+    def InitStateMachine(self):
         self.sm = smach.StateMachine(outcomes=['FINISH'])
 
         with self.sm:
-            smach.StateMachine.add('FACE_DETECTION', FaceDetection(self),
-                                  transitions={'worker': 'GESTURE_RECOGNITION',
-                                               'not_worker': 'FACE_DETECTION'})
-            smach.StateMachine.add('GESTURE_RECOGNITION', GestureRecognition(self),
-                                  transitions={'stop': 'PREPARE_FINISH',
-                                               'start': 'TOOL_RECOGNITION',
-                                               'not_gesture': 'GESTURE_RECOGNITION'})
-            smach.StateMachine.add('TOOL_RECOGNITION', ToolRecognition(self),
-                                  transitions={'num_1': 'GO_TASK',
-                                               'not_tool': 'TOOL_RECOGNITION'})
-            smach.StateMachine.add('GO_TASK', ExecuteTask(self),
-                                  transitions={'finish': 'PREPARE_FINISH',
-                                               'task_work_async': 'GO_TASK',
-                                               'stop': 'PAUSE'})
-            smach.StateMachine.add('PAUSE', Pause(),
-                                  transitions={'start': 'GO_TASK'})
-            smach.StateMachine.add('PREPARE_FINISH', PrepareFinish(self),
-                                  transitions={'success': 'FINISH'})
+            smach.StateMachine.add('FACE_DETECTION', FaceDetection(node=self),
+                                   transitions={'worker': 'GESTURE_RECOGNITION',
+                                                'not_worker': 'FACE_DETECTION'})
+            smach.StateMachine.add('GESTURE_RECOGNITION', GestureRecognition(node=self),
+                                   transitions={'stop': 'PREPARE_FINISH',
+                                                'start': 'TOOL_RECOGNITION',
+                                                'not_gesture': 'GESTURE_RECOGNITION'})
+            smach.StateMachine.add('TOOL_RECOGNITION', ToolRecognition(node=self),
+                                   transitions={'num_1': 'GO_TASK',
+                                                'not_tool': 'TOOL_RECOGNITION'})
+            smach.StateMachine.add('GO_TASK', ExecuteTask(node=self),
+                                   transitions={'finish': 'PREPARE_FINISH',
+                                                'task_work_async': 'GO_TASK',
+                                                'stop': 'PAUSE'})
+            smach.StateMachine.add('PAUSE', Pause(node=self),
+                                   transitions={'start': 'GO_TASK',
+                                                'not_gesture': 'PAUSE'})
+            smach.StateMachine.add('PREPARE_FINISH', PrepareFinish(node=self),
+                                   transitions={'success': 'FINISH'})
 
-       sis = smach_ros.IntrospectionServer('smach_introspection_server', self.sm, '/SM_ROOT')
-       sis.start()
-                               
-    
+    def StartStateMachin(self):
+        self.outcome = self.sm.execute()
+        self.sis = smach_ros.IntrospectionServer(
+            'smach_introspection_server', self.sm, '/SM_ROOT')
+        self.sis.start()
+
+    def StopStateMachin(self):
+        self.sis.stop()
+        self.sm.close()
+
 
 def main():
-    
+
     rclpy.init(args=None)
     node = MainNode()
 
     rclpy.spin(node)
+
 
 if __name__ == '__main__':
     main()
